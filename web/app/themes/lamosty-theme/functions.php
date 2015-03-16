d17dd9134e4d7adccf926dbfeef82a85b50bf995
Rastislav Lamos <lamos.rasto@gmail.com>
2015-03-16 18:59:44 +0100

    Switch to child theme

--- web/app/themes/lamosty-theme/functions.php
+++ web/app/themes/lamosty-theme/functions.php
@@ -1,4 +1,10 @@
 <?php
 /**
  * @ Lamosty.com 2015
- */ 
+ */
+
+add_action( 'wp_enqueue_scripts', 'lamosty_child_theme_scripts' );
+
+function lamosty_child_theme_scripts() {
+	wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+}
