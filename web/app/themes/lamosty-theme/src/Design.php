0f8e63f88e97a3c432b368c6b8040c8db5e0de40
Rastislav Lamos <lamos.rasto@gmail.com>
2015-03-17 17:56:27 +0100

    Remove Make credits

--- web/app/themes/lamosty-theme/src/Design.php
+++ web/app/themes/lamosty-theme/src/Design.php
@@ -5,12 +5,20 @@
 
 class Design {
 	public function filters_init() {
+		/*
+		 * Get rid of sidebars with zero widgets taking up space.
+		 */
 		add_filter( 'make_has_sidebar', array( $this, 'remove_inactive_sidebars' ), 10, 2 );
+
+		/*
+		 * Remove Make credits.
+		 */
+		add_filter( 'make_show_footer_credit', '__return_false' );
 	}
 
 
 	public function remove_inactive_sidebars( $show_sidebar, $location ) {
-		if ($location != 'right') {
+		if ( $location != 'right' ) {
 			return $show_sidebar;
 		}
 
