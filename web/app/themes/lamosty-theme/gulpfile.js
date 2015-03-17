fc465f054abce01996aa0fbbcac67270f5f7df8a
Rastislav Lamos <lamos.rasto@gmail.com>
2015-03-17 14:40:21 +0100

    Fix gulp tasks, add autoprefixer

--- web/app/themes/lamosty-theme/gulpfile.js
+++ web/app/themes/lamosty-theme/gulpfile.js
@@ -2,19 +2,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 var assetsPath = './assets';
 var destPath = './';
 
 gulp.task('sass', function () {
-    gulp.src(assetsPath + '/sass/**/*.scss')
-        .pipe(watch(assetsPath + '/sass/**/*.scss', function(files) {
-            return files
-                .pipe(sass())
-                .pipe(minifyCSS({keepBreaks:true}))
-                .pipe(gulp.dest(destPath));
-        }))
+    return gulp.src(assetsPath + '/sass/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(minifyCSS({keepBreaks:true}))
         .pipe(gulp.dest(destPath));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(assetsPath + '/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
