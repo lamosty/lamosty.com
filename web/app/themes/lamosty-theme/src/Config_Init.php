75b9ec42c6eecf429ccea3586eed36703c42e015
Rastislav Lamos <lamos.rasto@gmail.com>
2015-06-08 13:12:35 +0200

    Increase version of style.css

--- web/app/themes/lamosty-theme/src/Config_Init.php
+++ web/app/themes/lamosty-theme/src/Config_Init.php
@@ -6,6 +6,7 @@
 class Config_Init {
 
 	public function actions_init() {
+
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 	}
 
@@ -15,6 +16,9 @@ class Config_Init {
 
 	public function enqueue_styles() {
 		wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+
+         wp_register_style( 'ttfmake-main-style', get_stylesheet_directory_uri() . '/style.css',
+             array(), '1.0.1' );
 	}
 
 }
