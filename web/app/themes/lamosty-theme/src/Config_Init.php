1446a725d6c330a5914082a9f3c5aa1175954bf4
Rastislav Lamos <lamos.rasto@gmail.com>
2015-07-15 23:53:08 +0200

    Add GitHub profile social link

--- web/app/themes/lamosty-theme/src/Config_Init.php
+++ web/app/themes/lamosty-theme/src/Config_Init.php
@@ -8,6 +8,8 @@ class Config_Init {
 	public function actions_init() {
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+
+		add_filter( 'make_social_links', array( $this, 'make_social_links' ) );
 	}
 
 	public function enqueue_scripts() {
@@ -17,8 +19,18 @@ class Config_Init {
 	public function enqueue_styles() {
 		wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 
-         wp_register_style( 'ttfmake-main-style', get_stylesheet_directory_uri() . '/style.css',
-             array(), '1.0.1' );
+		wp_register_style( 'ttfmake-main-style', get_stylesheet_directory_uri() . '/style.css',
+			array(), '1.0.1' );
+	}
+
+	public function make_social_links( $services_with_links ) {
+		$services_with_links['github'] = array(
+			'title' => 'GitHub',
+			'url'   => 'https://github.com/lamosty',
+			'class' => 'fa-github',
+		);
+
+		return $services_with_links;
 	}
 
 }
